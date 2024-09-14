a62b0a20026ae3c6bca186b0ed48f4be67669827
Shiva4048 <sivasai.laveti@gmail.com>
2024-09-14 12:41:47 +0530

    Shell Script Updated

--- shell.sh
+++ shell.sh
@@ -3,12 +3,17 @@ read a
 echo "Enter a Number for B"
 read b
 
-let c =$((a+b))
+let c=$a+$b
 echo "The Sum of A & B is : $c"
 
-let d = $a - $b
+let d=$a-$b
 echo "The Subtraction of A & B is : $d"
-let e = $a * $b
+
+let e=$a*$b
 echo "The Multiplication os A & B is : $e"
-let f = $a / $b
-echo "The Division of the A & B is : $f"
+
+let f=$a/$b
+echo "The Diivision of the A & B is : $f"
+
+let g=$a%$b
+echo "The Modulus of theA & B is : $g"
