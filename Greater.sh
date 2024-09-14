ccfae58a9e5b38afbd8305ac901896b6b30624b7
Shiva4048 <Sivasai.Laveti@gmail.com>
2024-09-14 13:34:40 +0530

    Update Greater.sh

--- Greater.sh
+++ Greater.sh
@@ -5,13 +5,13 @@ read b
 echo "Enter Final Choice :"
 read c
 
-if [ $a -gt $b, $c ]; then 
+if [ $a -gt $b ] and [ $a -gt $c]
 echo "The Greatest Number is : $a"
 else
-if [ $b -gt $a, $c ]; then
+if [ $b -gt $a ] and [ $b -gt $c]
 echo "The Greatest Number is : $b"
 else
-if [ $c -gt $a, $b ]; then
+if [ $c -gt $a ] and [ $c -gt $b]
 echo "The Greatest Number is : $c"
 fi
 fi
